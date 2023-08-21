97152941f947b8fe9539caba5cb17527bbacf27d
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-08-21 11:50:41 +0200

    Add function creating tasks

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
+import { prisma } from "../db";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+    "use server"
+
+    const title = data.get("title")?.valueOf()
+    if(typeof title !== "string" || title.length === 0){
+        throw new Error("Invalid title")
+    }
+
+    await prisma.todo.create({data: {title, complete: false}})
+    redirect("/")
+}
 
 export default function Page(){
     return <>
     <header className="flex justify-between items-center mb-4 ">
         <h1 className="text-3xl">Dodaj zadanie</h1>
       </header>
-      <form className="flex gap-2 flex-col">
+      <form action={createTodo} className="flex gap-2 flex-col">
         <input type="text" 
         name="title" 
         className="text-3xl border border-slate-300 bg-transparent outline-none
