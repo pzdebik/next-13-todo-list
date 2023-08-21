ab51be0a4b7b38cc0c724aeac7270de02618db05
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-08-21 10:48:35 +0200

    Create function fetching todos from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home(){
-  return <h1>Hi</h1>
+import Link from "next/link";
+import { prisma } from "./db";
+
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+export default async function Home(){
+
+  const  todos = await getTodos()
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4 ">
+        <h1 className="text-2xl">Zadania</h1>
+        <Link 
+        className="border border-slate-300 text-slate-300 px-2 py-1 
+        rounded hover:bg-slate-700 focus-wthin:bg-slate-700 outline-none" 
+        href="/new">Dodaj zadanie</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo =>(
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
